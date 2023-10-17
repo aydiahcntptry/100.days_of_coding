a03b455af3da3caf7990ba1bee58e6edb06ef79e
Ayudiah Cinta Putry <147232785+aydiahcntptry@users.noreply.github.com>
2023-10-17 23:12:16 +0700

    Biodata

---
+++ Day1.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    
+    public static void main(String[] args) {
+        String nama = "Cinta";
+        String nim = "D0223019";
+        int angkatan = 2023;
+        System.out.println("nama :" + nama + "\n" + "nim :" + nim + "\n" + "angkatan :" + angkatan);
+        
+        
+    }
+    
+}
