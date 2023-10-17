8993d0a6253fac735e64003e4a708d4a1e81d4fa
Ayudiah Cinta Putry <147232785+aydiahcntptry@users.noreply.github.com>
2023-10-17 23:29:18 +0700

    OperatorPenugasan

---
+++ Day4.java
@@ -0,0 +1,34 @@
+
+public class OperatorPenugasan {
+    
+    public static void main(String[] args) {
+        int angkaZ;
+        int angkaA;
+        
+        angkaZ =20;
+        angkaA =10;
+        
+        
+        angkaA += angkaZ;
+        // angkaA = 30
+        System.out.println("penambahan : " + angkaA);
+        
+        
+        angkaA -= angkaZ;
+        // angkaA 30 - angkaZ 20 = angkaA 10
+        System.out.println("pengurangan : " + angkaA);
+        
+        angkaA *= angkaZ;
+        // angkaA 10 * angkaZ 20 = angkaA		 200
+        System.out.println("perkalian : " + angkaA);
+        
+        angkaA /= angkaZ;
+        // angkaA 200 / angkaZ 20 = angkaA 10
+        System.out.println("pembagian : " + angkaA);
+        
+        angkaA %= angkaZ;
+        // angkaA 10 % angkaZ 20 = angkaA 10 
+        System.out.println("sisabagi : " + angkaA);
+    }
+    
+}
