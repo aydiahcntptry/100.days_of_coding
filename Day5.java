b2f7c376cb21e6e974d61f6764155864716ee92c
Ayudiah Cinta Putry <147232785+aydiahcntptry@users.noreply.github.com>
2023-10-17 23:30:32 +0700

    OperatorPerbandingan

---
+++ Day5.java
@@ -0,0 +1,44 @@
+
+public class OperatorPerbandingan {
+    
+    public static void main(String[] args) {
+        int nilaiG = 4;
+        int nilaiJ = 9;
+        boolean hasil;
+        
+        //apakah nilaiG lebih besar nilaiJ?
+        hasil = nilaiG > nilaiJ;
+        System.out.println(hasil);
+        
+        
+        //apakah nilaiG lebih kecil nilaiJ?
+        hasil = nilaiG < nilaiJ;
+        System.out.println(hasil);
+        
+        
+        
+        //apakah nilaiG sama dengan nilaiJ?
+        hasil = nilaiG == nilaiJ;
+        System.out.println(hasil);
+        
+        
+        
+        //apakah nilaiG tidak sama dengan nilaiJ?
+        hasil = nilaiG != nilaiJ;
+        System.out.println(hasil);
+        
+        
+        
+        //apakah nilaiG lebih besar sama dengan nilaiJ?
+        hasil = nilaiG >= nilaiJ;
+        System.out.println(hasil);
+        
+        
+        
+        //apakah nilaiG lebih kecil sama dengan nilaiJ?
+        hasil = nilaiG <= nilaiJ;
+        System.out.println(hasil);
+        
+    }
+    
+}
