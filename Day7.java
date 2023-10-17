855129cb19120ba668ab961a8106adf049c47e9f
Ayudiah Cinta Putry <147232785+aydiahcntptry@users.noreply.github.com>
2023-10-17 23:35:41 +0700

    Percabangan perbandingan

---
+++ Day7.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int nilaiujian = 95;
+        
+        if (nilaiujian >= 70) {
+            System.out.println("Lulus");
+        } else {
+        System.out.println("tidak lulus");
+            
+        }       
+    }
+    
+}
