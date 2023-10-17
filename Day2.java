0721a9dab71851c0130c5a64fc9ae7a470a09ce5
Ayudiah Cinta Putry <147232785+aydiahcntptry@users.noreply.github.com>
2023-10-17 23:26:04 +0700

    Tipe Data

---
+++ Day2.java
@@ -0,0 +1,21 @@
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("tipe data angka bulat dan angka desimal");
+        long nilai = 7;
+        byte umur = 20;
+        short kuis = 80;
+        int angka = 100;
+        float bb = 50.3f;
+        double tinggi= 155.7;
+        System.out.println("nilai : " + nilai );
+        System.out.println("umur : " + umur + "tahun");
+        System.out.println("kuis : " + kuis);
+        System.out.println("angka : " + angka);
+        System.out.println("bb : " + bb + "kg");
+        System.out.println("tinggi: " + tinggi + "cm");
+    }
+    
+}
