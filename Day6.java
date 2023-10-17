25617c57f102caa8877e9da0516b482a8e6e2117
Ayudiah Cinta Putry <147232785+aydiahcntptry@users.noreply.github.com>
2023-10-17 23:31:53 +0700

    OperatorLogika

---
+++ Day6.java
@@ -0,0 +1,14 @@
+
+public class operatorlogika {
+    
+    public static void main(String[] args) {
+        int angka = 20;
+        int nilai = 10;
+        
+        System.out.println(angka==angka && angka!=nilai);
+        
+        System.out.println(angka!=angka || angka==nilai);
+            
+        }
+    }
+    
