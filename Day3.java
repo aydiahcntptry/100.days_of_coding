c40f220200837dd82abada38110310a69c2b2e9f
Ayudiah Cinta Putry <147232785+aydiahcntptry@users.noreply.github.com>
2023-10-17 23:28:07 +0700

    OperatorAritmatika

---
+++ Day3.java
@@ -0,0 +1,21 @@
+
+public class OperatorAritmatika {
+    
+    public static void main(String[] args) {
+        int angka = 4;
+        int nilai = 9;
+        
+        int tambah = angka + nilai;
+        int kurang = angka - nilai;
+        int bagi = angka / nilai;
+        int kali = angka * nilai;
+        int sisabagi = angka % nilai;
+        
+        System.out.println("tambah =" + tambah);
+        System.out.println("kurang =" + kurang);
+        System.out.println("bagi =" + bagi);
+        System.out.println("kali =" + kali);
+        System.out.println("sisabagi =" + sisabagi);
+    }
+    
+}	
