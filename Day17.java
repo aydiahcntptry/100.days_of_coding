b200b7146335994b6f4de522278aef0d569410c6
Ayudiah Cinta Putry <147232785+aydiahcntptry@users.noreply.github.com>
2023-10-25 19:49:01 +0700

    Angka berurutan

---
+++ Day17.java
@@ -0,0 +1,18 @@
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        //perulangan angka berurutan
+        
+        for (int i = 100; i >= 10; i--){
+            if (i % 2 == 0){
+                System.out.println(i);
+            }else{
+                System.out.println("angka yang tidak memenuhi syarat");
+            }
+        }
+        
+    }
+    
+}
