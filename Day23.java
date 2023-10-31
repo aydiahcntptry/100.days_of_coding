9675938159d91e57a9e31338ec0d421f5d728ae3
Ayudiah Cinta Putry <147232785+aydiahcntptry@users.noreply.github.com>
2023-11-01 00:55:36 +0700

    Angka yang hanya dapat di bagi 5

---
+++ Day23.java
@@ -0,0 +1,27 @@
+package bilangan.dapat.di.bagi.pkg5;
+
+public class BilanganDapatDiBagi5 {
+
+    public static void main(String[] args) {
+        
+        // hari ini kita belajar tentang
+        //bilangan angka 100 sampai 1 yang hanya dapat dibagi 5
+        //dari besar ke kecil
+        
+        for (int i = 100; i >= 1; i--) {
+            if (i % 5 ==0){
+                System.out.println(i); 
+                
+                //hasil outputnya hanya angka yang dapat di bagi 5 yaitu
+                //5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100
+                
+                //jika ingin angka yang hanya dapat di bagi angka yang lainnya, misalnya 2
+                //anda hanya perlu menggantikan 5 menjadi 2 dan hasil outputnya 
+                //adalah hanya angka dapat di bagi 2  
+                    
+                }
+            }
+            
+        }
+            
+        }
