1087d426fbbcae21c55132f03a11043f299b3c27
Ayudiah Cinta Putry <147232785+aydiahcntptry@users.noreply.github.com>
2023-11-02 20:19:05 +0700

    Belajar buat pola 3

---
+++ Day25.java
@@ -0,0 +1,16 @@
+package latihanbuatpola;
+public class LatihanBuatPola {
+    public static void main(String[] args) {
+        // latihan buat pola 3
+        
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= 5; j++) {
+                System.out.print("* ");
+                
+            }
+            System.out.println();
+            }
+        
+       
+        }
+    }
