575efb7c8dde69b36f2e6bc632036d74901d5cab
Ayudiah Cinta Putry <147232785+aydiahcntptry@users.noreply.github.com>
2023-11-03 23:05:26 +0700

    Pola

---
+++ Day26.java
@@ -0,0 +1,23 @@
+package latihan_pola;
+public class Latihan_Pola {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = x / 2;
+        for (int i = 1; i <= x; i++) {
+            if (i <= y) {
+                for (int j = 1; j <= i; j++) {
+                    System.out.print("* ");          
+                }
+            }else{
+                for (int j = 1; j <= x - i + 1; j++) {
+                    System.out.print("* ");
+                    
+                }
+            }
+            System.out.println();
+             
+        }
+    
+    }
+    
+}
