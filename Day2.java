9ac05d7793aff9d6787c8cb8f4a6cf96f41caa9c
Ayudiah Cinta Putry <147232785+aydiahcntptry@users.noreply.github.com>
2023-11-14 14:50:10 +0700

    Tipe data bulat & desimal

--- Day2.java
+++ Day2.java
@@ -1,20 +1,21 @@
 
-public class Main {
-    
+package pkg100.days.of.coding;
+
+public class TipeData {
+
     public static void main(String[] args) {
-        
-        System.out.println("tipe data angka bulat dan angka desimal");
-        long nilai = 7;
+       System.out.println("tipe data angka bulat dan angka desimal");
+        long nilai = 85;
         byte umur = 20;
         short kuis = 80;
         int angka = 100;
-        float bb = 50.3f;
+        float bb = 53.3f;
         double tinggi= 155.7;
         System.out.println("nilai : " + nilai );
         System.out.println("umur : " + umur + "tahun");
         System.out.println("kuis : " + kuis);
         System.out.println("angka : " + angka);
-        System.out.println("bb : " + bb + "kg");
+        System.out.println("berat badan : " + bb + "kg");
         System.out.println("tinggi: " + tinggi + "cm");
     }
     
