73466619d010c0ad3618548f2bd876997beadeba
Ayudiah Cinta Putry <147232785+aydiahcntptry@users.noreply.github.com>
2023-11-14 17:05:36 +0700

    percabangan if else (nilai ujian)

--- Day7.java
+++ Day7.java
@@ -1,15 +1,17 @@
 
-public class Main {
-    
+package pkg100.days.of.coding;
+
+public class Day7_percabangan_if_else {
+
     public static void main(String[] args) {
-        int nilaiujian = 95;
+        
+       int nilaiujian = 95;
         
         if (nilaiujian >= 70) {
             System.out.println("Lulus");
         } else {
         System.out.println("tidak lulus");
             
-        }       
     }
-    
+  }  
 }
