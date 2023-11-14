daee881b1edbc353a6958443afa93b64c5a8b008
Ayudiah Cinta Putry <147232785+aydiahcntptry@users.noreply.github.com>
2023-11-14 15:18:09 +0700

    Operator Logika

--- Day6.java
+++ Day6.java
@@ -1,14 +1,16 @@
 
-public class operatorlogika {
-    
+package pkg100.days.of.coding;
+
+public class Day6_operatorLogika {
+
     public static void main(String[] args) {
+
         int angka = 20;
         int nilai = 10;
         
         System.out.println(angka==angka && angka!=nilai);
-        
         System.out.println(angka!=angka || angka==nilai);
-            
-        }
+           
     }
     
+}
