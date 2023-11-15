9a03f97ef567dc30f32469f0ebadb56cbd5b0b36
Ayudiah Cinta Putry <147232785+aydiahcntptry@users.noreply.github.com>
2023-11-15 16:00:27 +0700

    percabangan if else (soal ke 2 ROUND 2)

--- Day8.java
+++ Day8.java
@@ -1,16 +1,23 @@
+
+package pkg100.days.of.coding;
+
 import java.util.Scanner;
-public class Main {
-	       public static void main(String[] args) {
-	           Scanner input=new Scanner(System.in);
-	           System.out.println("masukkan angka");
-		       int angka=input.nextInt( );
+public class Day8_soal2ROUND2 {
+
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+	 
+        System.out.print("masukkan angka : ");
+	int angka= sc.nextInt( );
 	           
-	           if(angka % 3 == 0 && angka % 5 == 0){
-	               System.out.println("master class");
-	           }else if (angka % 3 == 0){
-	               System.out.println("pride of 3");
-	       }else if(angka % 5 == 0){
-	           System.out.println("pride of 5");}
+	if(angka % 3 == 0 && angka % 5 == 0){
+	    System.out.println("master class");
+	}else if (angka % 3 == 0){
+	    System.out.println("pride of 3");
+	}else if(angka % 5 == 0){
+	    System.out.println("pride of 5");}
 	           
-	   }
+	   
+    }
+    
 }
