9d9b7b8dca79c2ea5f1f9af122033c86f463d93e
Ayudiah Cinta Putry <147232785+aydiahcntptry@users.noreply.github.com>
2023-11-18 06:40:33 +0700

    percabangan if else (restoran mawar)

--- Day34.java
+++ Day34.java
@@ -1,12 +1,11 @@
 
-package Day34;
+package pkg100.days.of.coding;
 
 import java.util.Scanner;
 
-public class RestoranMawar {
+public class Day34_restoranMawar {
 
     public static void main(String[] args) {
-        
         /* Restoran Mawar menawarkan diskon sebesar 20% untuk makanan 
            dan minuman pada hari Senin hingga Kamis. Jika seorang pelanggan 
            memesan makanan senilai Rp 150.000 pada hari Rabu, 
@@ -31,7 +30,7 @@ public class RestoranMawar {
 
         System.out.println("Total tagihan setelah diskon: Rp" + totalTagihan);
 
-
     }
     
 }
+    
