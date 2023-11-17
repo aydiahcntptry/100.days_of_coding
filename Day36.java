0f6877a80c0bdf20d62d3ec1f7a7b91d8b26fe64
Ayudiah Cinta Putry <147232785+aydiahcntptry@users.noreply.github.com>
2023-11-18 06:46:20 +0700

    pola 4

--- Day36.java
+++ Day36.java
@@ -1,12 +1,12 @@
 
-package day36;
+package pkg100.days.of.coding;
 
-public class Pola4 {
+public class Day36_pola4 {
 
     public static void main(String[] args) {
         // pola 4
         
-        int e = 4;
+        int e = 5;
         
         for (int i = 1; i <= e; i++) {
             for (int j = 1; j <= i; j++) {
