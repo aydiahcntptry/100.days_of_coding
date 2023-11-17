4c138d47ffbaaa9d218e819bb940e03f6cfe048d
Ayudiah Cinta Putry <147232785+aydiahcntptry@users.noreply.github.com>
2023-11-17 10:05:14 +0700

    ganjil & genap

--- Day15.java
+++ Day15.java
@@ -1,16 +1,18 @@
 
-public class Main {
-    
+package pkg100.days.of.coding;
+public class Day15_GanjilGenap {
+
     public static void main(String[] args) {
-        
-        for (int i = 1; i <= 10; i++){
-            if (i % 2 == 0) {
-               System.out.println(i + "genap"); 
+      // belajar ganjil dan genap
+      
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 1){
+                System.out.println(i + " ganjil");
             }
         }
-        for (int i = 1; i <= 10; i++){
-            if (i % 2 == 1){
-                System.out.println(i + "ganjil");
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0){
+                System.out.println(i + " genap");
             }
         }
     }
