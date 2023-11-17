adb652d61d13a07019392648c7a0172c6a8be2f7
Ayudiah Cinta Putry <147232785+aydiahcntptry@users.noreply.github.com>
2023-11-17 09:48:14 +0700

    Belajar Increment & Decremen

--- Day13.java
+++ Day13.java
@@ -1,8 +1,13 @@
 
-public class Main {
-    
+package pkg100.days.of.coding;
+
+public class Day13_
+package pkg100.days.of.coding;
+
+public class Day13_Increment_dan_Decrement {
+
     public static void main(String[] args) {
-        // belajar Increment dan Decrement
+       // belajar Increment dan Decrement
         
         
         System.out.println("___Increment___");
@@ -50,10 +55,60 @@ public class Main {
         
         System.out.println(g);
         System.out.println(h);
+
+    }  
+}
+ {
+
+    public static void main(String[] args) {
+       // belajar Increment dan Decrement
         
-        // sekian
         
+        System.out.println("___Increment___");
+        // Increment dibagi jadi dua yaitu 
         
-    }
-    
+        // 1. Post Increment = a++
+        System.out.println("Post Increment = a++");
+        //Post a = b; b = b + 5; 
+        int a;
+        int b = 5;
+        a = b++;
+        
+        System.out.println(a);
+        System.out.println(b);
+        
+        // 2. Pre Increment = ++a
+        System.out.println("Pre Increment = ++a");
+        //Pre d = d + d; c = 5;
+        int c;
+        int d = 5;
+        c = ++d;
+        
+        System.out.println(c);
+        System.out.println(d);
+        
+        System.out.println("___Decrement___");
+        // Decrement dibagi jadi dua yaitu 
+        
+        // 1. Post Decrement = a--
+        //Post e = f; f = f - 5;
+        System.out.println("Post Decrement = a--");
+        int e;
+        int f = 5;
+        e = f--;
+        
+        System.out.println(e);
+        System.out.println(f);
+        
+        // 2. Pre Decrement = --a
+        //Pre h = h - h; g = 5;
+        System.out.println("Pre Decrement = --a");
+        int g;
+        int h = 5;
+        g = --h;
+        
+        System.out.println(g);
+        System.out.println(h);
+
+    }  
 }
