58f5d779533b3cdfbcd8a36f91043a41295f8e2d
Ayudiah Cinta Putry <147232785+aydiahcntptry@users.noreply.github.com>
2023-11-17 15:41:20 +0700

    faktorial

--- Day18.java
+++ Day18.java
@@ -1,14 +1,17 @@
 
-public class Main {
-    
+package pkg100.days.of.coding;
+
+public class Day18_faktorial {
+
     public static void main(String[] args) {
+        
         int angka =7;
         int faktorial = 1;
         for (int i = 1; i <= angka; i++){
             faktorial *=i;
         }
-        System.out.println("hasil perkalian dari 1 hingga angka" + angka + "adalah" + faktorial);
-        //1*2*3*4*5*6*7=5040
+        System.out.println("hasil perkalian dari 1 hingga angka " + angka + " adalah " + faktorial);
+        // 1*2*3*4*5*6*7 = 5040
     }
     
 }
