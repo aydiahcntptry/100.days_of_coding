7ca9f18b35db5bdcab2e5a15dd2f3747f6c1df1b
Ayudiah Cinta Putry <147232785+aydiahcntptry@users.noreply.github.com>
2023-11-17 15:27:46 +0700

    memahami (%) modulus

--- Day16.java
+++ Day16.java
@@ -1,28 +1,27 @@
+ 
+public class Day16_memahami_modulus {
 
-public class perulangan {
-    
     public static void main(String[] args) {
         
-        // untuk for
-        //i = 2
-        // i lebih kecil sama dengan 20
-        // i+=3 yaitu 20 + 3 = 23 
-        //sekarang i nya adalah 23
-        // untuk if 
-        // i % 4 == 2 berarti 
-        // 23 % 4 == 2
-        /* 2 % 4 = 2
-           14 % 4 = 2 
-        */
-        // jadi maksudnya adalah sisa bagi yang hasilnya 2 !
-        
-        
+        // memahami (%)modulus atau sisa bagi :)
+       
         for (int i = 2; i <= 20; i+=3){
-            if (i % 4 == 2) {
+            if (i % 4 == 2 ) {
                 System.out.println(i);
+            }else{
+                System.out.println("angka tidak memenuhi syarat");
             }
         }
-        
+        // coba lagi
+        System.out.println("==========coba lagi==========");
+        for (int i = 0; i <= 10; i++) {
+            if (i % 2 == 1){
+                System.out.println(i);    
+            }else{
+                System.out.println("angka tidak memenuhi syarat");
+            }
+        }
     }
     
 }
+    
