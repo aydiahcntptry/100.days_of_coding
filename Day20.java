f639bc05ff44ad930927183f7f5fd43d123d9a4a
Ayudiah Cinta Putry <147232785+aydiahcntptry@users.noreply.github.com>
2023-11-19 21:14:47 +0700

    perulangan (kelipatan 5)

--- Day20.java
+++ Day20.java
@@ -1,12 +1,12 @@
 
 package pkg100.days.of.coding;
 
-public class Day20_jumlahKelibatan3 {
+public class Day20_jumlahKelipatan5 {
     public static void main(String[] args) {
         int jumlahKelipatan = 0;
         
         for (int i = 0; i <= 30; i++) {
-            if (i % 3 == 0) {
+            if (i % 5 == 0) {
                 System.out.println(i + " ");
 
             }
