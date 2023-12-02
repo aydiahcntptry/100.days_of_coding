f1a3e5778a6b40576446b9c1e0896d55175bad2c
Cinta^^ <147232785+aydiahcntptry@users.noreply.github.com>
2023-12-02 19:57:34 +0700

    mencetak isi array, while

---
+++ Day55.java
@@ -0,0 +1,18 @@
+
+package pkg100.days.of.coding;
+
+public class Day55_array {
+
+    public static void main(String[] args) {
+        
+        System.out.println("=====mencetak isi array menggunakan while=====");
+        
+        int angka [] = {23,24,25,26};
+        
+        int i = 0;
+        while(i<angka.length){
+            System.out.println(angka[i]);
+            i++;
+        }
+    }
+}
