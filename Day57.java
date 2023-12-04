57c12edbf7b86f938c8c988fa41cd70e32acaa8e
Cinta^^ <147232785+aydiahcntptry@users.noreply.github.com>
2023-12-04 21:52:29 +0700

    mencetak array terbalik, for

---
+++ Day57.java
@@ -0,0 +1,15 @@
+
+package pkg100.days.of.coding;
+
+public class Day57_array {
+
+    public static void main(String[] args) {
+       
+        String buah [] = {"mangga","apel","semangka","pisang"};
+        
+        for (int i = buah.length-1; i >= 0; i--) {
+            System.out.println("indeks ke " + i + " = " + buah[i]); 
+        }
+    }
+     
+}
