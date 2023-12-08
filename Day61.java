ad79f02bcb88f8f95a58d72b3c6928357e5f5705
Cinta^^ <147232785+aydiahcntptry@users.noreply.github.com>
2023-12-08 20:01:36 +0700

    menjumlahkan elemen array

---
+++ Day61.java
@@ -0,0 +1,17 @@
+
+package pkg100.days.of.coding;
+
+public class Day61_array {
+
+    public static void main(String[] args) {
+        
+        int angka [] = {70,75,80,85};
+        int hasil = 0;
+        
+        for (int i = 0; i < angka.length; i++) {
+            hasil = hasil + angka[i];
+                System.out.println(hasil);
+        }
+    }
+    
+}
