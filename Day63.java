147dd0f80dabb48633995d2582083524afec13c6
Cinta^^ <147232785+aydiahcntptry@users.noreply.github.com>
2023-12-10 20:27:33 +0700

    mengubah isi array

---
+++ Day63.java
@@ -0,0 +1,17 @@
+package pkg100.days.of.coding;
+
+import java.util.Arrays;
+
+public class Day63_array {
+
+    public static void main(String[] args) {
+        
+        String warna [] = {"merah","kuning","biru"};
+        
+        warna [2] = "hijau";
+        
+        System.out.println(Arrays.toString(warna));
+
+    }
+    
+}
