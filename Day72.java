b98f5c5bd75c05a89c5a2933775e01003e399069
Cinta^^ <147232785+aydiahcntptry@users.noreply.github.com>
2023-12-19 19:18:15 +0700

    input & cetak data mahasiswa, array2D

---
+++ Day72.java
@@ -0,0 +1,36 @@
+package pkg100.days.of.coding;
+
+import java.util.Scanner;
+
+public class Day72_array {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner (System.in);
+        
+        String dataMahasiswa [][] = new String [5][3]; 
+        
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 3; j++) {
+                if(j == 0){
+                    System.out.print("Nama : ");
+                }else if(j == 1){
+                    System.out.print("NIM : ");
+                }else{
+                    System.out.print("Nilai : ");
+                }
+                dataMahasiswa[i][j] = sc.next();
+            }
+            System.out.println("========================");
+        }
+        System.out.println("=====CETAK DATA MAHASISWA=====");
+        
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.println(dataMahasiswa[i][j] + " ");
+            }
+            System.out.println("");
+        }
+    }
+    
+}
