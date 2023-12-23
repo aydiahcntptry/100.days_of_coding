c1c847cce6d74f9c49629e1816d6572ba7d521fd
Cinta^^ <147232785+aydiahcntptry@users.noreply.github.com>
2023-12-23 21:32:05 +0700

    perulangan while (soal ke 1 ROUND 2)

---
+++ Day76.java
@@ -0,0 +1,27 @@
+package pkg100.days.of.coding;
+
+import java.util.Scanner;
+
+public class Day76_ {
+
+    public static void main(String[] args) {
+         
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("masukan angka : ");
+        int angka = sc.nextInt();
+        
+        while(angka % 2 == 0){
+            System.out.println("genap");
+            
+            System.out.print("masukan nilai :");
+            angka = sc.nextInt();
+            
+        while (angka % 2 == 1){
+            System.out.println("you and i, end"); 
+            break;
+        }    
+        }
+    }
+    
+}
