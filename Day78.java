b5415f6ca28f1d5835eef021bc6d232f87e110c9
Cinta^^ <147232785+aydiahcntptry@users.noreply.github.com>
2023-12-25 19:47:41 +0700

    mencetak nilai dalam array yang bernalai 0

---
+++ Day78.java
@@ -0,0 +1,27 @@
+package pkg100.days.of.coding;
+
+public class Day78_ {
+
+    public static void main(String[] args) {
+        
+        int[][] nilai = {
+        	{1, 0, 0}, 
+        	{0, 5, 6}, 
+        	{3, 1, 0}
+        	};
+        int jumlahNol = hitungJumlahNol(nilai);
+        System.out.println("Jumlah elemen array yang bernilai 0 : " + jumlahNol);
+    }
+
+    public static int hitungJumlahNol(int[][] array) {
+        int jumlah = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (array[i][j] == 0) {
+                    jumlah++;
+                }
+            }
+        }
+        return jumlah; 
+    }
+}
