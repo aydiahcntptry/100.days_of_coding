dbdd7c9400bab71a6900d77f2511d38ca8cf39e0
Cinta^^ <147232785+aydiahcntptry@users.noreply.github.com>
2023-12-26 19:29:55 +0700

    menampilkan angka terakhir di setiap baris, array2D

---
+++ Day79.java
@@ -0,0 +1,24 @@
+package pkg100.days.of.coding;
+
+public class Day79_ {
+
+    public static void main(String[] args) {
+        
+        int nilai [][] = {
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+        };
+        
+        int jumlahBaris = nilai.length;
+        int jumlahKolom = nilai[0].length;
+        
+        for (int i = 0; i < jumlahBaris; i++) {
+            int angkaTerakhir = nilai[i][jumlahKolom - 1 ];
+            System.out.println("angka terakhir di kolom ke : " + (i + 1) + " = " + angkaTerakhir);
+            
+        }
+    }
+    
+}
+
