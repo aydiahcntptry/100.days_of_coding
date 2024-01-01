ecb1085a15bbd305703d6e5e3283cd019bb48e64
Cinta^^ <147232785+aydiahcntptry@users.noreply.github.com>
2024-01-01 20:43:44 +0700

    memanggil method prosedur

---
+++ Day85.java
@@ -0,0 +1,19 @@
+package pkg100.days.of.coding;
+
+public class Day85_ {
+    
+    public static void mahasiswa(){
+        String nama = "Cinta";
+        int umur = 19;
+        String kelas = "informatika B";
+        System.out.println("Nama \t: " + nama );
+        System.out.println("Umur \t: " + umur + " tahun");
+        System.out.println("Kelas \t: " + kelas );
+    }
+
+    public static void main(String[] args) {
+      mahasiswa(); 
+    }
+    
+}
+
