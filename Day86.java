9ac7f56e381273424fb1f245f76731b30f58dda7
Cinta^^ <147232785+aydiahcntptry@users.noreply.github.com>
2024-01-02 09:43:29 +0700

    memanggil method fungsi

---
+++ Day86.java
@@ -0,0 +1,17 @@
+package pkg100.days.of.coding;
+
+public class Day86_ {
+    
+    public static int nilai(){
+        int a = 12;
+        int b = 8;
+        int c = 10;
+        return a+b-c;
+        
+    }
+    public static void main(String[] args) {
+        System.out.println(nilai());
+    }
+    
+}
+
