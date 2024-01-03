eaa703fb077222209d6e72289546b3110c51ab19
Cinta^^ <147232785+aydiahcntptry@users.noreply.github.com>
2024-01-03 17:58:07 +0700

    method parameter, aritmatika

---
+++ Day87.java
@@ -0,0 +1,17 @@
+package pkg100.days.of.coding;
+
+public class Day87_ {
+    
+    public static void aritmatika(int a, int b){
+        System.out.println(a + " + " + b + " = " + (a+b));
+        System.out.println(a + " - " + b + " = " + (a-b));
+        System.out.println(a + " x " + b + " = " + (a*b));
+        System.out.println(a + " : " + b + " = " + (a/b));
+ 
+    }
+    
+    public static void main(String[] args) {
+       aritmatika(30,10);
+    }
+    
+}
