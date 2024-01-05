7b66daa3cefdd4218697c5ef3eba2f08c55bd5b2
Cinta^^ <147232785+aydiahcntptry@users.noreply.github.com>
2024-01-05 21:08:32 +0700

    arraylist

---
+++ Day89.java
@@ -0,0 +1,44 @@
+package pkg100.days.of.coding;
+
+import java.util.ArrayList;
+
+public class Day89_ {
+
+    public static void main(String[] args) {
+        
+        ArrayList<String> warna = new ArrayList<>();
+        
+        warna.add("merah");
+        warna.add("putih");
+        warna.add("kuning");
+        warna.add("biru");
+        
+        // array awal
+        System.out.println("===ARRAY AWAL===");
+        System.out.println(warna);
+        
+        // tambah elemen array
+        System.out.println("===TAMBAH ELEMEN ARRAY===");
+        warna.add(2,"hijau");
+        System.out.println(warna);
+        
+        // menghapus elemen array
+        System.out.println("===HAPUS ELEMEN ARRAY===");
+        warna.remove("putih");
+        System.out.println(warna);
+        
+        // cetak array satu satu
+        System.out.println("===CETAK ARRAY SATU SATU===");
+        System.out.println(warna.get(0));
+        System.out.println(warna.get(1));
+        System.out.println(warna.get(2));
+        System.out.println(warna.get(3));
+        
+        //menghapus semua elemen array
+        System.out.println("===MENGHAPUS SEMUA ELEMEN ARRAY===");
+        warna.clear();
+        System.out.println(warna);
+    }
+    
+}
+
