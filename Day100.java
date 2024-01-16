f222f77b5ee6cea27c9a6dfe527d3cc8c8cf1e69
Cinta^^ <147232785+aydiahcntptry@users.noreply.github.com>
2024-01-16 19:08:46 +0700

    Palindrome

---
+++ Day100.java
@@ -0,0 +1,33 @@
+package pkg100.days.of.coding;
+
+import java.util.Scanner;
+
+public class Day100_ {
+
+    public static void main(String[] args) {
+        
+        /*mengecek kata/nomor palindrome atau bukan
+        kata palindrome adalah kata-kata yang dapat dibaca
+        baik dari depan maupun dari belakang*/
+        
+        String kata, balik = "";
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("masukan kata/nomor : ");
+        kata = sc.nextLine();
+        
+        int length = kata.length();
+        
+        for (int i = length -1; i >=0; i--) {
+            balik = balik + kata.charAt(i);
+        }
+        
+        if(kata.equals(balik)){
+            System.out.println(kata + " adalah kata/nomor palindrome");
+        }else{
+            System.out.println(kata + " adalah bukan kata/nomor Palindrome");
+        }
+        
+    }
+}
+    
